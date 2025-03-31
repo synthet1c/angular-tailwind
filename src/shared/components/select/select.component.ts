eee1c2759d790d7571bc43edcf82b5b0bde3b218
founts <founts24@gmail.com>
2025-03-31 17:28:31 +1100

    Add SelectComponent and SocketService with tests
    Introduced a new SelectComponent for sorting options with a dropdown UI and change detection optimizations. Additionally, implemented a SocketService to manage WebSocket communication using Socket.IO. Both components have accompanying unit tests to ensure functionality.

---
+++ src/shared/components/select/select.component.ts
@@ -0,0 +1,40 @@
+import {ChangeDetectionStrategy, Component, inject, OnInit} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {NgForOf} from '@angular/common';
+import {path, prop, sortBy, descend} from 'rambda';
+import {Chat} from '#models';
+import {ChatService} from '#services/chat/chat.service';
+
+@Component({
+  selector: 'app-select',
+  imports: [
+    FormsModule,
+    NgForOf
+  ],
+  templateUrl: './select.component.html',
+  styleUrl: './select.component.css',
+  standalone: true,
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class SelectComponent implements OnInit {
+
+  chatService = inject(ChatService);
+
+  public sortingOptions = [
+    Chat.Sort.RECOMMENDED,
+    Chat.Sort.OLDEST,
+    Chat.Sort.NEWEST,
+    Chat.Sort.MOST_EXPENSIVE,
+    Chat.Sort.LEAST_EXPENSIVE,
+  ]
+
+  ngOnInit() {
+    console.log('SelectComponent:onInit');
+  }
+
+  sort(event: Event) {
+    const selectElement = event.target as HTMLSelectElement;
+    console.log('SelectComponent:sort', event);
+    this.chatService.sort(selectElement.value as Chat.Sort);
+  }
+}
---
+++ src/shared/services/socket/socket.service.ts
@@ -0,0 +1,45 @@
+import { Injectable, PLATFORM_ID, Inject } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { io, Socket } from 'socket.io-client';
+import {Observable} from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class SocketService {
+  private readonly socket!: Socket;
+
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {
+    if (isPlatformBrowser(this.platformId)) {
+      // Initialize the socket if running on the browser
+      this.socket = io('http://localhost:8080/chats');
+    }
+  }
+
+  emit(event: string, data: any): void {
+    if (this.socket) {
+      this.socket.emit(event, data);
+    }
+  }
+
+  get<T = unknown>(event: string, data: any): Observable<T> {
+    return new Observable((subscriber) => {
+      this.socket.emit(event, data, (result: T) => {
+        subscriber.next(result);
+        subscriber.complete();
+      });
+    });
+  }
+
+  post(event: string, data: any): void {
+    if (this.socket) {
+      this.socket.emit(event, data);
+    }
+  }
+
+  onMessage(event: string, callback: (data: any) => void): void {
+    if (this.socket) {
+      this.socket.on(event, callback);
+    }
+  }
+}
